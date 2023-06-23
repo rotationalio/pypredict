305979ec6b2c7bcb41a2b79566d4fa49165803bd
Patrick Deziel <deziel.patrick@gmail.com>
2023-06-23 12:06:14 -0500

    initial version bump v0.1.0

---
+++ pypredict/version.py
@@ -0,0 +1,32 @@
+##########################################################################
+## Module Info
+##########################################################################
+
+__version_info__ = {
+    "major": 0,
+    "minor": 1,
+    "micro": 0,
+    "releaselevel": "final",
+    "serial": 0,
+}
+
+##########################################################################
+## Helper Functions
+##########################################################################
+
+
+def get_version(short=False):
+    """
+    Returns the version string for pyensign.
+    """
+    assert __version_info__["releaselevel"] in ("alpha", "beta", "final")
+    vers = [
+        "%(major)i.%(minor)i" % __version_info__,
+    ]
+    if __version_info__["micro"]:
+        vers.append(".%(micro)i" % __version_info__)
+    if __version_info__["releaselevel"] != "final" and not short:
+        vers.append(
+            "%s%i" % (__version_info__["releaselevel"][0], __version_info__["serial"])
+        )
+    return "".join(vers)
